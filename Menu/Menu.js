c083ba67cbc584141f114efc22e0cf9beb1bb851
Irving Duran <ijd.irving@gmail.com>
2019-03-06 17:18:04 -0400

    finals

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -4,12 +4,10 @@ const toggleMenu = () => {
   menu.classList.toggle('menu--open');
 };
 
-// toggleMenu() {
-//   menu.classList.toggle('menu--open');
-// };
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
+console.log(menu)
 
 // create a reference to the ".menu-button" class
 const menuButton = document.querySelector('.menu-button');
